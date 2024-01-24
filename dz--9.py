2e5df33a05d712251ae104a8cc8bdd01bb26e850
nataliia7575 <150849227+nataliia7575@users.noreply.github.com>
2024-01-24 14:17:18 +0200

    Update dz--9.py

--- dz--9.py
+++ dz--9.py
@@ -1,74 +1,64 @@
 phone_book = dict()   
 
+def handler_add(name, number):
+    phone_book[name] = number
+    return
+
+def handler_phone(name):
+    for k, v in phone_book.items():
+        if k == name:  
+            print(f'This is phone for {k} to call: {v}')            
+    return
+
+def handler_change(name, new_number):
+    if name in phone_book:
+        phone_book[name] = new_number
+        return       
+    raise KeyError
+
 def input_error(func):
     def inner(*args, **qwargs):
         try:
             result = func(*args, **qwargs)
             return result
         except KeyError:
-            print("There is no such name in a phone_book. Start again") 
-            return 
-
+            print("There is no such name in a phone book. Start again") 
+        except ValueError:
+            print("Input valid user name and phone")
     return inner
 
-def message_parser(input_message):
-    name, phone = input_message.split()
-    return name, phone
-
-def handler_add():
-    try:
-        input_message = input("Input new contact: name and phone \n")
-        name, phone = message_parser(input_message)
-        phone_book[name] = phone
-    except ValueError:
-        print("Input valid user name and phone")
-    return
-
-def handler_phone():
-    input_message = input("Input contact name to get a phone to call:\n")
-    name = input_message
-    for k, v in phone_book.items():
-        if k == name:  
-            print(f'This is phone for {k} to call {v}')            
-    return
-
 @input_error
-def handler_change():
-    try:
-        input_message = input("Input contact name and new phone to be changed:\n")
-        name, phone = message_parser(input_message)
-        print(f'Changing phone for {phone_book[name]}')
-        phone_book[name] = phone   
-    except ValueError:
-        print("Input valid user name and phone: ")             
-    return
+def parser(input_command, input_list):
+    if  input_command == 'HELLO'.lower():
+        print("How can I help you?\n")
+        input_command = input("Input a command from the list: add ..., change ..., phone ..., show all\n")
+            
+    if input_command == 'ADD'.lower():
+        result = handler_add(name=input_list[1], number=input_list[2])
+                
+    if  input_command == 'CHANGE'.lower():
+        result = handler_change(name=input_list[1], new_number=input_list[2])
+
+    if input_command == 'PHONE'.lower():
+        result = handler_phone(name=input_list[1])
+                        
+    if input_command == 'SHOW'.lower():
+        print('Phone book: ', phone_book)
+    
+    return result
 
 def main():
     while True:
-        input_command = input("Input a command from the list: hello, add, change, phone, show all\n").lower()
+        input_command = input("Input a command from the list: hello, add ..., change ..., phone ..., show all\n")
+        input_list = input_command.split()
 
-        if input_command == 'HELLO'.lower():
-            print("How can I help you?\n")
-            input_command = input("Input a command from the list: add, change, phone, show all\n")
-            
-        if input_command == 'ADD'.lower():
-            result= handler_add()
-                   
-        if input_command == 'CHANGE'.lower():
-            result= handler_change()
-
-        if input_command == 'PHONE'.lower():
-            result= handler_phone()
-                           
-        if input_command == 'SHOW ALL'.lower():
-            print('Phone book: ', phone_book)
+        input_command = input_list[0]
 
         if input_command == 'GOOD BYE'.lower() or input_command == 'CLOSE'.lower() or input_command == 'EXIT'.lower():
             print('Good bye!')
             break
-    return result
-
-print(main())
+        else:
+            print(parser(input_command, input_list))  
 
 
 
